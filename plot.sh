d23f6fdeb1821f9660172a1b4e240cb1f0981637
Matt Bunney <matt_bunney@live.com.au>
2022-04-07 11:31:15 +1000

    fixed a pesky minus sign

--- plot.sh
+++ plot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-count=15
+count=10
 for (( i = 0; i < $count; ++i )); do
     echo $( echo "-0.5 * $i / $count" | bc -l ) >> tp_vals
-    julia -t 2 src/include.jl --nk 16 --nkf 19 --tp $( echo "-0.5 * $i / $count" | bc -l ) --mu $( echo "2 * $i / $count " | bc -l) --U 3 >> lambda_vals
+    julia -t 2 src/include.jl --nk 12 --nkf 15 --tp $( echo "-0.5 * $i / $count" | bc -l ) --mu $( echo "-2 * $i / $count " | bc -l) --U 3 >> lambda_vals
 done
 
 # python3 src/plot.py
